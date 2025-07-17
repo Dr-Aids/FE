bd13c5bc20deab5af1d0d9780d47e63cc211fa96
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-07-17 21:05:32 +0900

    [#1] feat: Record 컴포넌트 생성

--- src/pages/Patient/components/Record.tsx
+++ src/pages/Patient/components/Record.tsx
@@ -1,1 +1,25 @@
-export default function Record() {}
+import Button from "../../../components/Button";
+import "./Record.css";
+
+export default function Record({
+  role,
+  content,
+}: {
+  role: string;
+  content: string;
+}) {
+  return (
+    <div className="record__container">
+      <div className="record__header">
+        <div className="record__title">
+          {role === "nurse" ? "Nurse Records" : "Doctor Orders"}
+        </div>
+        <div className="record__buttons">
+          <Button content={"삭제"} />
+          <Button content={"수정"} />
+        </div>
+      </div>
+      <div className="record__content">{content}</div>
+    </div>
+  );
+}
