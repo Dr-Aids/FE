69e770c2bb78fd00b0dbb60380613c5ada1ed0a8
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-07-17 21:09:30 +0900

    [#1] feat: 환자 페이지 초기UI 구현

--- src/pages/Patient/PatientPage.tsx
+++ src/pages/Patient/PatientPage.tsx
@@ -1,3 +1,4 @@
+import Record from "./components/Record";
 import WeightBox from "./components/WeightBox";
 import "./PatientPage.css";
 
@@ -7,17 +8,22 @@ export default function PatientPage() {
       <div className="patient__page__weight_list">
         <WeightBox title="Pre-Weight" weight={58.9} />
         <WeightBox title="Average-Weight" weight={57.9} />
-        <WeightBox title="Pre-Weight" weight={59} />
-        <WeightBox title="Pre-Weight" weight={59} />
-        <WeightBox title="Pre-Weight" weight={59} />
+        <WeightBox title="Dry-Weight" weight={59} />
+        <WeightBox title="Target UF" weight={59} />
+        <WeightBox title="Post-Weight" weight={59} />
       </div>
-
-      <div className="patient__page__graph">그래프</div>
+      <div className="patient__page__graph">
+        *선택 회차의 최대 이전 5회까지만 표시됩니다.
+        <div className="patient__page__graph_container">
+          <div>그래프 1</div>
+          <div>그래프 2</div>
+        </div>
+      </div>
       <div className="patient__page__bp__container">
         <div>혈압</div>
         <div className="patient__page__record__container">
-          <div className="patient__page__nurse">Nurse 레코드</div>
-          <div className="patient__page__doctor">Doctor 레코드</div>
+          <Record role={"nurse"} content={"간호사"} />
+          <Record role={"doctor"} content={"의사"} />
         </div>
       </div>
     </div>
