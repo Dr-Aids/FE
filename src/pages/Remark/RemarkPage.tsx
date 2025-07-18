00a703f748ba82d1dcc2ce40280846521af6b397
unknown <jhje5595@naver.com>
2025-07-18 22:03:08 +0900

    [#4] feat: 특이사항 페이지 초기 레이아웃 설정

--- src/pages/Remark/RemarkPage.tsx
+++ src/pages/Remark/RemarkPage.tsx
@@ -1,4 +1,21 @@
+import PatientListPage from "../../components/PatientListPage";
+import PatientSummaryCard from "../../components/PatientSummaryCard.tsx";
 import "./RemarkPage.css";
 export default function RemarkPage() {
-  return <div className="remark-page__container">특이사항</div>;
+  return (
+    <div className="remark-page__container">
+      <div className="remark__main__content">
+        <PatientSummaryCard
+          name="정연준"
+          age={18}
+          birth="2002.07.31"
+          sex="남"
+          disease={"당뇨병성 신종"}
+          pageName="환자 정보"
+        />
+        특이사항 오는 곳
+      </div>
+      <PatientListPage />
+    </div>
+  );
 }
