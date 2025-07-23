10feb7242cc0b7f4b598f2e8afb3f934cfa2d721
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-07-24 00:46:59 +0900

    [#2] feat: 카드 컴포넌트 생성

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,5 +1,11 @@
+import SectionHeader from "../../components/SectionHeader";
 import "./MainPage.css";
 
 export default function MainPage() {
-  return <div className="mainpage__container">메인</div>;
+  return (
+    <div className="mainpage__container">
+      <SectionHeader title={"AI 요약 및 응답"} />
+      <hr />
+    </div>
+  );
 }
---
+++ src/pages/Main/components/MainCard.tsx
@@ -0,0 +1,5 @@
+import "./MainCard.css";
+
+export default function () {
+  return <div className='main__card__cnotainer'>메인 카드 컴포넌트</div>;
+}
