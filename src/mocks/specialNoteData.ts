8a48793c12f6e0a28d5160da2fdceb2c22720056
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-07-26 20:28:53 +0900

    [#11] chore: 기존 임시 data 수정

--- src/mocks/specialNoteData.ts
+++ src/mocks/specialNoteData.ts
@@ -1,160 +1,38 @@
 export const specialNoteData = [
   // 1개 특이사항
   {
+    doctor: "김세희",
     patientName: "김지민",
     sessionInfo: "1회차 25.04.10",
-    type: "몸무게",
-    currentNote: "평균 증가 몸무게 + 1.2kg",
-    previousNote: "x",
-  },
-
-  // 2개 특이사항
-  {
-    patientName: "박서준",
-    sessionInfo: "2회차 25.04.12",
-    type: "몸무게",
-    currentNote: "이전회차 Post + 3.6kg",
-    previousNote: "이전회차 Post + 2.8kg",
+    type: "체중",
+    status: "이전 투석 후 몸무게보다 5kg 증가",
   },
   {
-    patientName: "박서준",
-    sessionInfo: "2회차 25.04.12",
+    doctor: "김세희",
+    patientName: "김지민",
+    sessionInfo: "1회차 25.04.10",
+    type: "체중",
+    status: "이전 투석 후 몸무게보다 5kg 증가",
+  },
+  {
+    doctor: "김세희",
+    patientName: "김지민",
+    sessionInfo: "1회차 25.04.10",
+    type: "체중",
+    status: "이전 투석 후 몸무게보다 5kg 증가",
+  },
+  {
+    doctor: "김세희",
+    patientName: "김지민",
+    sessionInfo: "1회차 25.04.10",
+    type: "체중",
+    status: "이전 투석 후 몸무게보다 5kg 증가",
+  },
+  {
+    doctor: "김세희",
+    patientName: "김지민",
+    sessionInfo: "1회차 25.04.10",
     type: "혈압",
-    currentNote: "투석 시작 혈압 160/95\n2단계 고혈압",
-    previousNote: "투석 시작 혈압 155/92\n1단계 고혈압",
-  },
-
-  // 3개 특이사항
-  {
-    patientName: "최민서",
-    sessionInfo: "3회차 25.04.14",
-    type: "혈압",
-    currentNote: "투석 중 혈압이 32 감소\n현재: 118/64",
-    previousNote: "투석 시작 혈압 150/90",
-  },
-  {
-    patientName: "최민서",
-    sessionInfo: "3회차 25.04.14",
-    type: "몸무게",
-    currentNote: "평균 증가 몸무게 + 1.8kg",
-    previousNote: "평균 증가 몸무게 + 1.0kg",
-  },
-  {
-    patientName: "최민서",
-    sessionInfo: "3회차 25.04.14",
-    type: "혈압",
-    currentNote: "투석 시작 혈압 170/100\n2단계 고혈압",
-    previousNote: "위 예시",
-  },
-
-  // 4개 특이사항
-  {
-    patientName: "이서윤",
-    sessionInfo: "4회차 25.04.16",
-    type: "혈압",
-    currentNote: "투석 시작 혈압 181/100\n3단계 고혈압",
-    previousNote: "x",
-  },
-  {
-    patientName: "이서윤",
-    sessionInfo: "4회차 25.04.16",
-    type: "몸무게",
-    currentNote: "이전회차 Post + 3.9kg",
-    previousNote: "이전회차 Post + 3.1kg",
-  },
-  {
-    patientName: "이서윤",
-    sessionInfo: "4회차 25.04.16",
-    type: "몸무게",
-    currentNote: "평균 증가 몸무게 + 1.5kg",
-    previousNote: "x",
-  },
-  {
-    patientName: "이서윤",
-    sessionInfo: "4회차 25.04.16",
-    type: "혈압",
-    currentNote: "투석 중 혈압이 30 감소\n현재: 115/65",
-    previousNote: "투석 시작 혈압 145/85",
-  },
-
-  // 5개 특이사항
-  {
-    patientName: "정하늘",
-    sessionInfo: "5회차 25.04.18",
-    type: "몸무게",
-    currentNote: "평균 증가 몸무게 + 2.1kg",
-    previousNote: "x",
-  },
-  {
-    patientName: "정하늘",
-    sessionInfo: "5회차 25.04.18",
-    type: "몸무게",
-    currentNote: "이전회차 Post + 4.1kg",
-    previousNote: "이전회차 Post + 3.5kg",
-  },
-  {
-    patientName: "정하늘",
-    sessionInfo: "5회차 25.04.18",
-    type: "혈압",
-    currentNote: "투석 시작 혈압 185/105\n3단계 고혈압",
-    previousNote: "START BP 170/100",
-  },
-  {
-    patientName: "정하늘",
-    sessionInfo: "5회차 25.04.18",
-    type: "혈압",
-    currentNote: "투석 중 혈압이 40 감소\n현재: 120/60",
-    previousNote: "x",
-  },
-  {
-    patientName: "정하늘",
-    sessionInfo: "5회차 25.04.18",
-    type: "혈압",
-    currentNote: "BP Drop 35mmHg",
-    previousNote: "Drop 28mmHg",
-  },
-
-  // 6개 특이사항
-  {
-    patientName: "윤도윤",
-    sessionInfo: "6회차 25.04.20",
-    type: "몸무게",
-    currentNote: "Post +4.3kg",
-    previousNote: "Post +3.6kg",
-  },
-  {
-    patientName: "윤도윤",
-    sessionInfo: "6회차 25.04.20",
-    type: "몸무게",
-    currentNote: "평균 증가 몸무게 + 2.0kg",
-    previousNote: "1.2kg",
-  },
-  {
-    patientName: "윤도윤",
-    sessionInfo: "6회차 25.04.20",
-    type: "혈압",
-    currentNote: "START BP 185/110\n매우 높음",
-    previousNote: "START BP 180/100",
-  },
-  {
-    patientName: "윤도윤",
-    sessionInfo: "6회차 25.04.20",
-    type: "혈압",
-    currentNote: "BP Drop 33mmHg",
-    previousNote: "Drop 25mmHg",
-  },
-  {
-    patientName: "윤도윤",
-    sessionInfo: "6회차 25.04.20",
-    type: "혈압",
-    currentNote: "투석 중 혈압이 38 감소\n현재: 110/60",
-    previousNote: "x",
-  },
-  {
-    patientName: "윤도윤",
-    sessionInfo: "6회차 25.04.20",
-    type: "혈압",
-    currentNote: "투석 시작 혈압 185/110\n3단계 고혈압",
-    previousNote: "투석 시작 혈압 175/95",
+    status: "이전 투석 후 몸무게보다 5kg 증가",
   },
 ];
