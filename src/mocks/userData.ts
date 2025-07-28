789c6f4778ca0de99aaa8b40e9b6dca02837b36b
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-07-28 14:46:18 +0900

    [#6] feat: 임시 userData 추가

---
+++ src/mocks/userData.ts
@@ -0,0 +1,12 @@
+export const userData = [
+  {
+    username: "정연준",
+    email: "jhje5595@naver.com",
+    password: "dbqlanghks0731!",
+  },
+  {
+    username: "최용재",
+    email: "choi@naver.com",
+    password: "good",
+  },
+];
