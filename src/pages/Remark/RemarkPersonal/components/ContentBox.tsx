f9b6ba86bb44703d611aafb73dbd1d42507fcd75
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-09 18:18:00 +0900

    [#21] style: 박스 디자인 변경

--- src/pages/Remark/RemarkPersonal/components/ContentBox.tsx
+++ src/pages/Remark/RemarkPersonal/components/ContentBox.tsx
@@ -2,8 +2,8 @@ import "./ContentBox.css";
 export default function ContentBox({ title, content }) {
   return (
     <div className="content__box__container">
-      {title}
-      {content}
+      <b>{title}</b>
+      <b>{content}</b>
     </div>
   );
 }
