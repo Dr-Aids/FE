2110c84c7a9a720c736171327688f4d6c10c6371
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-12 19:28:14 +0900

    [#29] feat: 현재회차,이전회차에 맞게 동적 렌더링되도록 변경

--- src/pages/RemarkPersonal/RemarkPersonalPage.tsx
+++ src/pages/RemarkPersonal/RemarkPersonalPage.tsx
@@ -9,9 +9,12 @@ export default function RemarkPersonalPage() {
   const patientId = useParams().patientId;
   const round = useParams().round;
   const patient = patientList.filter((p) => p.id === patientId)[0];
-  const roundData = patient.rounds.filter(
+  const nowRound = patient.rounds.filter(
     (r) => r.round.toString() === round?.toString()
   )[0];
+  const prevRound = patient.rounds.filter(
+    (r) => r.round.toString() === (Number(round) - 1).toString()
+  )[0];
 
-  return <RemarkPersonalDetails />;
+  return <RemarkPersonalDetails nowRound={nowRound} prevRound={prevRound} />;
 }
--- src/pages/RemarkPersonal/components/RemarkPersonalDetails.tsx
+++ src/pages/RemarkPersonal/components/RemarkPersonalDetails.tsx
@@ -1,26 +1,36 @@
+import BloodLineChart from "../../../components/BloodLineChart";
+import type { PatientRoundData } from "../../../types/patientSummaryType";
 import ContentBox from "./ContentBox";
 import "./RemarkPersonalDetails.css";
 import RemarkPersonalTable from "./RemarkPersonalTable";
 import WeightCMPChart from "./WeightCMPChart";
 
-export default function RemarkPersonalDetails() {
+export default function RemarkPersonalDetails({
+  nowRound,
+  prevRound,
+}: {
+  nowRound: PatientRoundData;
+  prevRound: PatientRoundData | null;
+}) {
   return (
     <div className="remark__personal__content__container">
       <div>
-        <RemarkPersonalTable />
+        <RemarkPersonalTable nowRound={nowRound} prevRound={prevRound} />
       </div>
       <div className="remark__personal__graphs__container">
-        <div>
+        <div className="blood__history__container">
           혈압기록
-          {/* <BloodHistoryChart data={} /> */}
+          <BloodLineChart data={nowRound.bloodPressure} />
         </div>
-        <div>
+        <div className="content__box__list">
+          현재회차
           <ContentBox title={"내원 시 BP"} content={"170/100"} />
           <ContentBox title={"현재 BP"} content={"136/76"} />
           <ContentBox title={"평균 몸무게에 비해"} content={"+1.8 kg"} />
           <ContentBox title={"현재 몸무게"} content={"58 kg"} />
         </div>
-        <div>
+        <div className="weight__cmp__container">
+          몸무게 비교
           <WeightCMPChart />
         </div>
       </div>
