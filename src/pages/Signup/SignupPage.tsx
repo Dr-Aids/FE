f37374da8c24a35172db3aba56de4f343874c9e3
unknown <jhje5595@naver.com>
2025-08-17 16:01:04 +0900

    [#34] feat: formData 입력값 검증 구현

--- src/pages/Signup/SignupPage.tsx
+++ src/pages/Signup/SignupPage.tsx
@@ -4,19 +4,98 @@ import LoginLogo from "../../assets/login-logo.png";
 import RoleButton from "./components/RoleButton";
 import { useState } from "react";
 
+interface FormErrors {
+  email?: string;
+  password?: string;
+  role?: string;
+  name?: string;
+  hospital?: string;
+}
+
+interface FormData {
+  email?: string;
+  password?: string;
+  role?: string;
+  name?: string;
+  hospital?: string;
+}
+
+const hospitalList = ["희승병원", "세종병원", "서울병원"];
+
 export default function SignupPage() {
-  const [isDoctor, setIsDoctor] = useState(true);
-  const handleClickDoctorButton = () => {
-    if (!isDoctor) {
-      // 이미 Doctor일 때 Doctor를 한 번 더 클릭하면 불필요한 렌더링 되는거 방지
-      setIsDoctor(true);
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+    role: "",
+    name: "",
+    hospital: "",
+  });
+  const [errors, setErrors] = useState<FormErrors>({});
+
+  const handleInputChage = (field: keyof FormData, value: string) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+    // 입력 시 해당 필드의 에러 메시지 제거
+    if (errors[field]) {
+      setErrors((prev) => ({ ...prev, [field]: undefined }));
     }
   };
-  const handleClickNurseButton = () => {
-    if (isDoctor) {
-      // 이미 Nurse 선택일 때 한 번 더 클릭하면 불필요한 렌더링 되는거 방지
-      setIsDoctor(false);
+
+  const checkField = () => {
+    const newErrors: FormErrors = {};
+    if (!formData.email) {
+      newErrors.email = "이메일을 입력해주세요";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = "올바른 이메일 형식을 입력해주세요.";
     }
+
+    if (!formData.password) {
+      newErrors.password = "비밀번호를 입력해주세요";
+    }
+    if (!formData.role) {
+      newErrors.role = "역할을 선택해주세요";
+    }
+    if (!formData.name) {
+      newErrors.name = "이름을 입력해주세요";
+    }
+    if (!formData.hospital) {
+      newErrors.hospital = "병원을 선택해주세요";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!checkField()) {
+      return;
+    }
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://draids.site/join", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            username: formData.name,
+            password: formData.password,
+            email: formData.email,
+            role: formData.role,
+            hospitalname: formData.hospital,
+          }),
+          credentials: "include",
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("API 요청 에러:", error);
+      }
+    };
+
+    fetchData();
   };
 
   return (
@@ -27,32 +106,83 @@ export default function SignupPage() {
 
       <main className="signup__main">
         <img src={LoginLogo} alt="Logo" />
-        <form className="signup__form">
-          <input placeholder="Email" />
-          <input type="password" placeholder="Password" />
+
+        <form className="signup__form" onSubmit={handleSubmit}>
+          {/*이메일*/}
+          <div className="email__container">
+            <p>이메일</p>
+            <input
+              placeholder="Email"
+              value={formData.email}
+              autoComplete="email"
+              onChange={(e) => handleInputChage("email", e.target.value)}
+              style={{ border: errors.email ? "1px solid red" : "" }}
+            />
+            {errors.email && <span>{errors.email}</span>}
+          </div>
+
+          {/*비밀번호*/}
+          <div className="password__container">
+            <p>비밀번호</p>
+            <input
+              type="password"
+              placeholder="Password"
+              autoComplete="new-password"
+              value={formData.password}
+              onChange={(e) => handleInputChage("password", e.target.value)}
+              style={{ border: errors.password ? "1px solid red" : "" }}
+            />
+            {errors.password && <span>{errors.password}</span>}
+          </div>
+
+          {/*역할 선택*/}
           <div className="role__container">
-            Role
+            역할
             <div className="role__btn__container">
               <RoleButton
                 role={"Doctor"}
-                isSelcetd={isDoctor}
-                onClick={handleClickDoctorButton}
+                isSelected={formData.role === "doctor"}
+                onClick={() => handleInputChage("role", "doctor")}
               />
               <RoleButton
                 role={"Nurse"}
-                isSelcetd={!isDoctor}
-                onClick={handleClickNurseButton}
+                isSelected={formData.role === "nurse"}
+                onClick={() => handleInputChage("role", "nurse")}
               />
             </div>
+            {errors.role && <span>{errors.role}</span>}
           </div>
 
-          <input placeholder="Name" />
-          <select className="signup__selcet__container">
-            <option>Hospital</option>
-            <option>희승병원</option>
-            <option>서울병원</option>
-            <option>세종병원</option>
-          </select>
+          {/*이름*/}
+          <div className="name__container">
+            <p>이름</p>
+            <input
+              placeholder="Name"
+              value={formData.name}
+              onChange={(e) => handleInputChage("name", e.target.value)}
+              style={{ border: errors.name ? "1px solid red" : "" }}
+            />
+
+            {errors.name && <span>{errors.name}</span>}
+          </div>
+
+          {/*병원*/}
+          <div className="select__container">
+            <p>병원</p>
+            <select
+              className="signup__selcet"
+              value={formData.hospital}
+              onChange={(e) => handleInputChage("hospital", e.target.value)}
+            >
+              <option selected disabled hidden>
+                Hospital
+              </option>
+              {hospitalList.map((item) => (
+                <option key={`SignUp-Hospital-${item}`}>{item}</option>
+              ))}
+            </select>
+            {errors.hospital && <span>{errors.hospital}</span>}
+          </div>
           <Button children={"Sign Up"} />
         </form>
       </main>
