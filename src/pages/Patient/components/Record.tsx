6be9940691c7e6ea929559f5ec8cd70c5d08d7d2
unknown <jhje5595@naver.com>
2025-08-21 03:04:56 +0900

    [#39] style: Record 디자인 변경

--- src/pages/Patient/components/Record.tsx
+++ src/pages/Patient/components/Record.tsx
@@ -1,25 +1,17 @@
 import Button from "../../../components/ui/Button";
 import "./Record.css";
 
-export default function Record({
-  role,
-  content,
-}: {
-  role: string;
-  content: string;
-}) {
+export default function Record() {
   return (
     <div className="record__container">
       <div className="record__header">
-        <div className="record__title">
-          {role === "nurse" ? "Nurse Records" : "Doctor Orders"}
-        </div>
+        <div className="record__title">Records</div>
         <div className="record__buttons">
           <Button content={"삭제"} />
           <Button content={"수정"} />
         </div>
       </div>
-      <div className="record__content">{content}</div>
+      <div className="record__content"></div>
     </div>
   );
 }
