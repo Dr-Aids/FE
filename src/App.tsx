a26b77a5944a5f28faaefeba315c6f023c44540c
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-20 20:20:51 +0900

    [#34] feat: AuthProvier 전역 적용

--- src/App.tsx
+++ src/App.tsx
@@ -10,42 +10,39 @@ import SettingPage from "./pages/Settings/SettingPage";
 import MainLayout from "./layouts/MainLayout";
 import SignupPage from "./pages/Signup/SignupPage";
 import RemarkPersonalPage from "./pages/RemarkPersonal/RemarkPersonalPage";
-import { useState } from "react";
 import MainContentWithPatient from "./layouts/MainContentWithPatient";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(false);
-  const [userData, setUserData] = useState(null); //로그인한 유저의 정보를 담을거임
-
   return (
     <>
       <GlobalStyles />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <LoginPage setUserData={setUserData} setIsLogin={setIsLogin} />
-          }
-        />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route element={<MainLayout userData={userData} />}>
-          <Route path="main" element={<MainPage />} />
-          <Route element={<MainContentWithPatient />}>
-            {/*여기부터는 우측에 환자 리스트 뜨는애들 */}
-            <Route path="remark" element={<RemarkPage />} />
-            <Route path="patient/:patientId/:round" element={<PatientPage />} />
-            <Route
-              path="prescription/:patientId/:round"
-              element={<Prescription />}
-            />
-            <Route
-              path="remark/:patientId/:round"
-              element={<RemarkPersonalPage />}
-            />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route element={<MainLayout />}>
+            <Route path="main" element={<MainPage />} />
+            <Route element={<MainContentWithPatient />}>
+              {/*여기부터는 우측에 환자 리스트 뜨는애들 */}
+              <Route path="remark" element={<RemarkPage />} />
+              <Route
+                path="patient/:patientId/:round"
+                element={<PatientPage />}
+              />
+              <Route
+                path="prescription/:patientId/:round"
+                element={<Prescription />}
+              />
+              <Route
+                path="remark/:patientId/:round"
+                element={<RemarkPersonalPage />}
+              />
+            </Route>
+            <Route path="settings" element={<SettingPage />} />
           </Route>
-          <Route path="settings" element={<SettingPage {...userData} />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </AuthProvider>
     </>
   );
 }
