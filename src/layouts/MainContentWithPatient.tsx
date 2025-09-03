e0992f31580d01ed1a53b1146085998a28482be0
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-04 01:25:08 +0900

    feat: 특이사항 전체 페이지에서 환자 개별정보 나오지 않도록 변경

--- src/layouts/MainContentWithPatient.tsx
+++ src/layouts/MainContentWithPatient.tsx
@@ -1,16 +1,14 @@
 import "./MainContentWithPatient.css";
 import PatientSummaryCard from "../components/PatientSummaryCard.tsx";
 import { Outlet, useLocation, useParams } from "react-router-dom";
-import { patients } from "../mocks/patientData.ts";
 import PatientListPage from "../components/PatientListPage";
 import SectionHeader from "../components/SectionHeader.tsx";
 
 export default function MainContentWithPatient() {
-  const { patientId, round } = useParams<{
-    patientId?: string;
-    round?: string;
+  const { patientId, session } = useParams<{
+    patientId: string;
+    session: string;
   }>();
-  const patient = patients.find((p) => p.id === patientId);
 
   const location = useLocation();
   const pageName = location.pathname.split("/")[1];
@@ -18,10 +16,8 @@ export default function MainContentWithPatient() {
   return (
     <div className="with-patient-layout__container">
       <div className="with-patient-layout__main__content">
-        {patient ? (
-          <SectionHeader title={pageName}>
-            <PatientSummaryCard />
-          </SectionHeader>
+        {!patientId && !session ? (
+          <SectionHeader title={pageName}></SectionHeader>
         ) : (
           <SectionHeader title={pageName}>
             <PatientSummaryCard />
