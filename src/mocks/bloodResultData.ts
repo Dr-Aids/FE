a622aeb2c5a952618dfcb86bd5d4271e56ebc0b9
unknown <jhje5595@naver.com>
2025-09-06 01:19:55 +0900

    chore: mock data 삭제 및 일부 수정

--- src/mocks/bloodResultData.ts
+++ src/mocks/bloodResultData.ts
@@ -5,19 +5,6 @@ export const bloodResultData = [
     Ferritine: 60,
     TIBC: 90,
     PTH: 70,
-  },
-  {
-    name: "2월",
-    Iron: 60,
-    Ferritine: 70,
-    TIBC: 80,
-    PTH: 50,
-  },
-  {
-    name: "3월",
-    Iron: 75,
-    Ferritine: 80,
-    TIBC: 70,
-    PTH: 60,
+    hematocrit: 10,
   },
 ];
