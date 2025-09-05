2c4f88093414c8872b105736b151e029cdd9617e
unknown <jhje5595@naver.com>
2025-09-06 01:48:06 +0900

    [#42] feat: 혈액검사,헤모글로빈 타입 지정

--- src/types/PrescriptionTypes.ts
+++ src/types/PrescriptionTypes.ts
@@ -5,3 +5,19 @@ export interface Prescription {
   id: number;
   iu: number;
 }
+
+export interface BloodResult {
+  id: number;
+  date: string;
+  iron: number;
+  ferritine: number;
+  hematocrit: number;
+  tibc: number;
+  pth: number;
+}
+
+export interface Hb {
+  id: number;
+  hemoglobin: number;
+  date: string;
+}
