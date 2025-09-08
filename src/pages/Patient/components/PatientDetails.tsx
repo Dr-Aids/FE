9a50db2e20411fb550a0e1816de938f873d15496
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-09 02:38:15 +0900

    [#44] feat: 체중정보 수정 버튼 추가

--- src/pages/Patient/components/PatientDetails.tsx
+++ src/pages/Patient/components/PatientDetails.tsx
@@ -3,16 +3,20 @@ import PRELineChart from "../components/PRELineChart";
 import Record from "./Record";
 import BarGraph from "./BarGraph";
 import WeightBox from "./WeightBox";
-import Button from "../../../components/ui/Button";
 import BloodLineChart from "../../../components/BloodLineChart";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
 import type {
   Bp,
   BpNote,
   FiveSessionWeightList,
   WeightListItem,
 } from "../../../types/PatientDetailTypes";
+import EditButton from "../../../components/ui/EditButton";
+import TrashButton from "../../../components/ui/TrashButton";
+import Modal from "../../../components/Modal";
+import WeightInput from "./WeightInput";
 
 export default function PatientDetails() {
   const { patientId, session } = useParams<{
@@ -25,8 +29,8 @@ export default function PatientDetails() {
     FiveSessionWeightList[] | null
   >();
   const [records, setRecords] = useState<BpNote[]>([]);
-
   const [bp, setBp] = useState<Bp | null>();
+  const [openWeightModal, setOpenWeightModal] = useState<boolean>(false);
 
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
@@ -128,15 +132,24 @@ export default function PatientDetails() {
   return (
     <div className="patient-details__container">
       {weightList ? (
-        <div className="patient__page__weight_list">
-          <WeightBox title="Pre-Weight" weight={weightList.preWeight} />
-          <WeightBox
-            title="Average-Weight"
-            weight={weightList?.averageWeight}
-          />
-          <WeightBox title="Dry-Weight" weight={weightList?.dryWeight} />
-          <WeightBox title="Target UF" weight={weightList?.targetUF} />
-          <WeightBox title="Post-Weight" weight={weightList?.postWeight} />
+        <div className="weight__wrapper">
+          <span className="weight__header">
+            <span>체중 정보</span>
+            <div className="weight__buttons">
+              <EditButton onClick={() => setOpenWeightModal(true)} />
+              <TrashButton />
+            </div>
+          </span>
+          <div className="patient__page__weight_list">
+            <WeightBox title="Pre-Weight" weight={weightList.preWeight} />
+            <WeightBox
+              title="Average-Weight"
+              weight={weightList?.averageWeight}
+            />
+            <WeightBox title="Dry-Weight" weight={weightList?.dryWeight} />
+            <WeightBox title="Target UF" weight={weightList?.targetUF} />
+            <WeightBox title="Post-Weight" weight={weightList?.postWeight} />
+          </div>
         </div>
       ) : (
         <div>몸무게 불러오는중...</div>
@@ -163,14 +176,27 @@ export default function PatientDetails() {
           <div className="patient__page__bp__chart__header">
             혈압
             <div className="patient__page__bp__buttons">
-              <Button content={"삭제"} />
-              <Button content={"수정"} />
+              <EditButton />
+              <TrashButton />
             </div>
           </div>
           <BloodLineChart data={bp} />
         </div>
         <Record records={records} />
       </div>
+
+      <Modal
+        title="체중 정보 수정"
+        isOpen={openWeightModal}
+        onClose={() => setOpenWeightModal(false)}
+      >
+        <WeightInput
+          patientId={patientId!}
+          session={session!}
+          weightList={weightList!}
+          onClose={() => setOpenWeightModal(false)}
+        />
+      </Modal>
     </div>
   );
 }
