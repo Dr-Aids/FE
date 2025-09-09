e4cbbc923bd0a1d6c839b86732e7c7df7532a087
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-09 23:40:22 +0900

    [#44] feat: 혈압 노트 등록 기능 추가

--- src/pages/Patient/components/Record.tsx
+++ src/pages/Patient/components/Record.tsx
@@ -1,12 +1,30 @@
-import type { BpNote } from "../../../types/PatientDetailTypes";
+import { useState } from "react";
+import Modal from "../../../components/Modal";
+import PlusButton from "../../../components/ui/PlusButton";
+import type { Bp, BpNote } from "../../../types/PatientDetailTypes";
 import "./Record.css";
 import RecordRow from "./RecordRow";
+import RecordInput from "./RecordInput";
 
-export default function Record({ records }: { records: BpNote[] }) {
+interface RecordProps {
+  records: BpNote[];
+  bps: Bp[];
+  patientId: string;
+  session: string;
+}
+
+export default function Record({
+  records,
+  bps,
+  patientId,
+  session,
+}: RecordProps) {
+  const [openRecordModal, setOpenRecordModal] = useState<boolean>(false);
   return (
     <div className="record__container">
       <div className="record__header">
         <div className="record__title">Records</div>
+        <PlusButton onClick={() => setOpenRecordModal(true)} />
       </div>
       <div className="record__content">
         {records?.length !== 0 ? (
@@ -29,6 +47,18 @@ export default function Record({ records }: { records: BpNote[] }) {
           <span>기록이 존재하지 않습니다.</span>
         )}
       </div>
+
+      <Modal
+        title="혈압 노트 등록"
+        isOpen={openRecordModal}
+        onClose={() => setOpenRecordModal(false)}
+      >
+        <RecordInput
+          onClose={() => setOpenRecordModal(false)}
+          bps={bps}
+          session={session}
+        />
+      </Modal>
     </div>
   );
 }
