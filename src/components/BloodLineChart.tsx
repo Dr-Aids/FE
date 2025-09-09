d7bf734987f93150dcb3d61898bf27be1feb5375
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-09 23:37:29 +0900

    [#44] feat: 시간 내부 변환에서 변환된 시간으로 받도록 변경

--- src/components/BloodLineChart.tsx
+++ src/components/BloodLineChart.tsx
@@ -7,18 +7,13 @@ import {
   Legend,
   Line,
 } from "recharts";
-import { formatYMDTHM } from "../utils/formatYMDTHM";
+
 import type { Bp } from "../types/PatientDetailTypes";
 
 export default function BloodLineChart({ data }: { data: Bp[] }) {
-  const newData = data.map((item) => ({
-    ...item,
-    time: formatYMDTHM(item.time),
-  }));
-
   return (
     <div className="blood__chart__container">
-      <LineChart width={470} height={280} data={newData}>
+      <LineChart width={470} height={280} data={data}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="time" />
         <YAxis />
