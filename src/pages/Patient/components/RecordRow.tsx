9298f3a688bc53f2a6d244c3cca5303ed96b3dff
unknown <jhje5595@naver.com>
2025-09-10 01:22:49 +0900

    [#44] feat: 삭제 기능 추가

--- src/pages/Patient/components/RecordRow.tsx
+++ src/pages/Patient/components/RecordRow.tsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
 import EditButton from "../../../components/ui/EditButton";
 import TrashButton from "../../../components/ui/TrashButton";
 import type { BpNote } from "../../../types/PatientDetailTypes";
 import "./RecordRow.css";
+import Modal from "../../../components/Modal";
+import RecordInput from "./RecordInput";
 
-export default function RecordRow({ time, isChecked, author, note }: BpNote) {
+interface RecordRowProps {
+  bpNote: BpNote;
+  session: string;
+}
+
+export default function RecordRow({ bpNote, session }: RecordRowProps) {
+  const [openModifyRecord, setOpenRecordModifyRecord] =
+    useState<boolean>(false);
+  const { time, isChecked, author, note } = bpNote;
+
+  const handleClickDeleteRecord = async () => {
+    if (!confirm("정말로 삭제하시겠습니까?")) return;
+    const accessToken = localStorage.getItem("accessToken");
+    try {
+      if (!accessToken)
+        throw new Error("잘못된 접근입니다 - 로그인 후 시도해주세요");
+      const res = await fetch(
+        `/api/blood-pressure/notes?pressureId=${bpNote.bloodPressureId}`,
+        {
+          method: "DELETE",
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
+      );
+      if (!res.ok) throw new Error(`HTTP Error - ${res.status}`);
+      const data = await res.json();
+      console.log(data.message);
+    } catch (err) {
+      console.log("에러메세지(혈압 노트 삭제) : ", err);
+    }
+  };
   return (
     <div className="recordrow__container">
       <div className="recordrow__info">
-        <input type="checkbox" checked={isChecked} />
+        <input type="checkbox" checked={isChecked} readOnly />
         <span>{time}</span>
         <span style={{ color: "#6F9EA6", fontWeight: "bold", flexShrink: 0 }}>
           {author}
@@ -23,9 +55,21 @@ export default function RecordRow({ time, isChecked, author, note }: BpNote) {
         </span>
       </div>
       <span className="recordrow__buttons">
-        <EditButton />
-        <TrashButton />
+        <EditButton onClick={() => setOpenRecordModifyRecord(true)} />
+        <TrashButton onClick={handleClickDeleteRecord} />
       </span>
+
+      <Modal
+        title="혈압 노트 수정"
+        isOpen={openModifyRecord}
+        onClose={() => setOpenRecordModifyRecord(false)}
+      >
+        <RecordInput
+          bpNote={bpNote}
+          onClose={() => setOpenRecordModifyRecord(false)}
+          session={session}
+        />
+      </Modal>
     </div>
   );
 }
