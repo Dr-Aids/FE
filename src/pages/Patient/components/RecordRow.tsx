12e50d6740655617893f7f60a909af0e675cdc89
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-09 23:39:54 +0900

    [#44] feat: 타임스탬프 형식 변경 및 버튼 우측 정렬

--- src/pages/Patient/components/RecordRow.tsx
+++ src/pages/Patient/components/RecordRow.tsx
@@ -1,26 +1,27 @@
 import EditButton from "../../../components/ui/EditButton";
 import TrashButton from "../../../components/ui/TrashButton";
 import type { BpNote } from "../../../types/PatientDetailTypes";
-import { formatYMDTHM } from "../../../utils/formatYMDTHM";
 import "./RecordRow.css";
 
 export default function RecordRow({ time, isChecked, author, note }: BpNote) {
   return (
     <div className="recordrow__container">
-      <input type="checkbox" checked={isChecked} />
-      <span>{formatYMDTHM(time)}</span>
-      <span style={{ color: "#6F9EA6", fontWeight: "bold", flexShrink: 0 }}>
-        {author}
-      </span>
-      <span
-        style={{
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-        }}
-      >
-        {note}
-      </span>
+      <div className="recordrow__info">
+        <input type="checkbox" checked={isChecked} />
+        <span>{time}</span>
+        <span style={{ color: "#6F9EA6", fontWeight: "bold", flexShrink: 0 }}>
+          {author}
+        </span>
+        <span
+          style={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {note}
+        </span>
+      </div>
       <span className="recordrow__buttons">
         <EditButton />
         <TrashButton />
