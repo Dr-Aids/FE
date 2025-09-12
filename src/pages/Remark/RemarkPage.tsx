7d41eb49811145fa392350861406670c8b8ba052
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-13 00:52:47 +0900

    [#49] feat: 오늘의 특이사항이 존재하지 않을 시 데이터 없음 컴포넌트 보여주도록 변경

--- src/pages/Remark/RemarkPage.tsx
+++ src/pages/Remark/RemarkPage.tsx
@@ -6,6 +6,7 @@ import RemarkBasis from "./components/RemarkBasis";
 import { useEffect, useState } from "react";
 import type { RemarkPatient } from "../../types/RemarkTypes";
 import ruleNameToText from "../../utils/ruleNameToText";
+import EmptyDataState from "../../components/EmptyDataState";
 
 const TypeCell = ({ type }: { type: string }) => (
   <span className="type__container">
@@ -42,42 +43,50 @@ export default function RemarkAllPatients() {
   return (
     <div className="remark__all__container">
       <div className="card-container">
-        <table className="remark__table">
-          <thead className="remark__thead">
-            <tr>
-              <th scope="col">
-                <h3>담당의</h3>
-              </th>
-              <th scope="col">
-                <h3>환자명</h3>
-              </th>
-              <th scope="col">
-                <h3>회차/날짜</h3>
-              </th>
-              <th scope="col">
-                <h3>종류</h3>
-              </th>
-              <th scope="col">
-                <h3>상태</h3>
-              </th>
-            </tr>
-          </thead>
-          <tbody className="remark__tbody">
-            {remarkPatients.map((data) => (
-              <tr key={`remark-all-patients-${data.id}`}>
-                <td>{data.picname}</td>
-                <td>{data.patientName}</td>
-                <td>
-                  {data.session}회차 / {data.date.replaceAll("-", ".")}
-                </td>
-                <td>
-                  <TypeCell type={data.type} />
-                </td>
-                <td>{ruleNameToText(data)}</td>
+        {remarkPatients.length === 0 ? (
+          <EmptyDataState
+            type="specialNote"
+            title="특이사항이 존재하지 않습니다"
+            description=""
+          />
+        ) : (
+          <table className="remark__table">
+            <thead className="remark__thead">
+              <tr>
+                <th scope="col">
+                  <h3>담당의</h3>
+                </th>
+                <th scope="col">
+                  <h3>환자명</h3>
+                </th>
+                <th scope="col">
+                  <h3>회차/날짜</h3>
+                </th>
+                <th scope="col">
+                  <h3>종류</h3>
+                </th>
+                <th scope="col">
+                  <h3>상태</h3>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="remark__tbody">
+              {remarkPatients.map((data) => (
+                <tr key={`remark-all-patients-${data.id}`}>
+                  <td>{data.picname}</td>
+                  <td>{data.patientName}</td>
+                  <td>
+                    {data.session}회차 / {data.date.replaceAll("-", ".")}
+                  </td>
+                  <td>
+                    <TypeCell type={data.type} />
+                  </td>
+                  <td>{ruleNameToText(data)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
       <div className="remark__basis__container">
         <RemarkBasis isWeight={false} />
